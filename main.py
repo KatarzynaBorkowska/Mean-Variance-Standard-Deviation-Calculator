c850caf54ed264ab74c2bf0182b65aa44e3381b8
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2023-02-27 14:37:06 +0100

    zrobiłam warunek zwrotu błędu, oraz 3x3 tablicę w np

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello ponownie")
-print(2)
+import numpy as np
+
+def calculate(axis):
+    if len(axis) != 9:
+        return "List must contain nine numbers."
+    arr = np.array(axis)
+    matrix = arr.reshape((3,3))
+    return matrix
+
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
