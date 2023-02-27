045cabf4ba5e56d1fc421835ddc80f85040e14a1
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2023-02-27 14:44:19 +0100

    zrobiony słownik z mean oraz variance

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-def calculate(axis):
-    if len(axis) != 9:
+def calculate(list):
+    if len(list) != 9:
         return "List must contain nine numbers."
-    arr = np.array(axis)
+    arr = np.array(list)
     matrix = arr.reshape((3,3))
-    return matrix
+    wynik = {
+        "mean": [ matrix.mean(axis=0).tolist(), matrix.mean(1).tolist() ,matrix.mean()],
+        "variance": [np.var(matrix, 0).tolist(), np.var(matrix, 1).tolist(), np.var(matrix)]
+    }
+    return wynik
 
 print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
